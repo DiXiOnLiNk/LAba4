cfe922caa172d6121490b068445db2a6e6b92a35
Andriy <dixionlink@gmail.com>
2024-03-17 13:49:46 +0200

    add tests

--- test2.py
+++ test2.py
@@ -1,48 +1,50 @@
 import unittest
-from calculactor import Calculator
+from calculactor import Calculator  # Припускаю, що клас Calculator знаходиться в файлі calculator.py
 
 class TestCalculator(unittest.TestCase):
-    def test_addition(self):
+    def test_add(self):
         calc = Calculator(5, 3)
         self.assertEqual(calc.add(), 8)
-        calc = Calculator(10.5, -2.5)
-        self.assertEqual(calc.add(), 8)
 
-    def test_subtraction(self):
-        calc = Calculator(10, 4)
-        self.assertEqual(calc.sub(), 6)
-        calc = Calculator(7.5, -2.5)
-        self.assertEqual(calc.sub(), 10)
+    def test_sub(self):
+        calc = Calculator(5, 3)
+        self.assertEqual(calc.sub(), 2)
 
-    def test_multiplication(self):
-        calc = Calculator(3, 4)
-        self.assertEqual(calc.mul(), 12)
-        calc = Calculator(-5, -2)
-        self.assertEqual(calc.mul(), 10)
+    def test_mul(self):
+        calc = Calculator(5, 3)
+        self.assertEqual(calc.mul(), 15)
 
-    def test_division(self):
-        calc = Calculator(10, 5)
+    def test_div(self):
+        calc = Calculator(6, 3)
         self.assertEqual(calc.div(), 2)
-        calc = Calculator(8, 0)
-        self.assertEqual(calc.div(), "Помилка: Ділення на нуль!")
+
+    def test_div_by_zero(self):
+        calc = Calculator(6, 0)
+        self.assertEqual(calc.div(), "Error: Division by zero!")
 
     def test_power(self):
         calc = Calculator(2, 3)
         self.assertEqual(calc.power(), 8)
+
+    def test_power_with_zero_exponent(self):
         calc = Calculator(5, 0)
         self.assertEqual(calc.power(), 1)
 
     def test_gcd(self):
         calc = Calculator(24, 36)
         self.assertEqual(calc.gcd(), 12)
-        calc = Calculator(17, 23)
-        self.assertEqual(calc.gcd(), 1)
 
     def test_lcm(self):
-        calc = Calculator(6, 8)
-        self.assertEqual(calc.lcm(), 24)
-        calc = Calculator(15, 25)
-        self.assertEqual(calc.lcm(), 75)
+        calc = Calculator(24, 36)
+        self.assertEqual(calc.lcm(), 72)
+
+    def test_lcm_with_negative_numbers(self):
+        calc = Calculator(-24, 36)
+        self.assertEqual(calc.lcm(), 72)
+
+    def test_lcm_with_zero(self):
+        calc = Calculator(24, 0)
+        self.assertEqual(calc.lcm(), 0)
 
 if __name__ == '__main__':
     unittest.main()
